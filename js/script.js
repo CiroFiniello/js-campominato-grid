37ec4b497735fcb1d4171836522138fdf35aac67
CodeKiro <kirofiniello@outlook.it>
2024-05-21 00:52:51 +0200

    finally the squareelement have been added to the page with js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 const gridElement = document.querySelector('section.grid');
+
+
+for (let index = 0; index < 100; index++) {
+    const squareElement = document.createElement('article');
+    squareElement.classList.add('square');
+    gridElement.appendChild(squareElement);
+}
