18bbcfb037d5fb24f8d655dee8e9b3e3ff109f91
CodeKiro <kirofiniello@outlook.it>
2024-05-21 01:33:39 +0200

    content in the squares have been added trouth an appendchild, now is time to make of odd or even

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const gridElement = document.querySelector('section.grid');
 
 
-for (let index = 0; index < 100; index++) {
+for (let index = 1; index < 101; index++) {
     const squareElement = document.createElement('article');
     squareElement.classList.add('square');
-    gridElement.appendChild(squareElement);
+
+    const contentEL = document.createElement('span');
+    contentEL.classList.add('content');
+    contentEL.append(index);
+    squareElement.appendChild(contentEL)
+    
+    squareElement.addEventListener('click', function(){
+        squareElement.classList.add('active');
+    });
+     gridElement.appendChild(squareElement);
 }
